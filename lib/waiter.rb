83e58c090a81fed4153cccf24685003d6f3c804a
jmhar <jmhar@protonmail.com>
2021-09-09 16:58:11 +1000

    restaurant things

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,28 @@
 class Waiter
+    attr_reader :name, :experience
 
+    @@all = []
+  
+    def initialize name, experience
+      @name = name
+      @experience = experience
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+
+    def new_meal customer, total, tip
+        Meal.new self, customer, total, tip
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        best_tip = meals.max_by {|meal| meal.tip}
+        best_tip.customer
+    end
 end
